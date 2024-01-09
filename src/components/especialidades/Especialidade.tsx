f4ad34aa0222ce142c12bfdf028a4b898cb0464e
Bruno Santos <bruno_st17@hotmail.com>
2024-01-08 21:27:12 -0300

    feat: adicionando o componente pricipal das especialidades

--- src/components/especialidades/Especialidade.tsx
+++ src/components/especialidades/Especialidade.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material';
 import Button from '@mui/material/Button';
+import Link from 'next/link';
 
 import Layout from '../layout/Layout';
 
@@ -8,9 +9,11 @@ function Especialidade() {
     <Layout title="Especialidades">
       <Box display={'flex'} justifyContent={'space-between'}>
         <div>Especialidades</div>
-        <Button variant="contained" style={{ backgroundColor: '#659e6d' }}>
-          Nova Especialidade
-        </Button>
+        <Link href={'/medical/especialidades/nova-especialidade'}>
+          <Button variant="contained" style={{ backgroundColor: '#659e6d' }}>
+            Nova Especialidade
+          </Button>
+        </Link>
       </Box>
     </Layout>
   );
