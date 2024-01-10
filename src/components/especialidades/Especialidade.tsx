5b1e45cdb58f588336d1012050794d662c21e968
Bruno Santos <bruno_st17@hotmail.com>
2024-01-09 22:38:52 -0300

    feat: adicionando o componente responsavel pelas especialidades

--- src/components/especialidades/Especialidade.tsx
+++ src/components/especialidades/Especialidade.tsx
@@ -1,20 +1,48 @@
-import { Box } from '@mui/material';
 import Button from '@mui/material/Button';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
+import { Especialidade as EspecialidadeModel } from '../../models/especialidade/especialidadeModel';
 import Layout from '../layout/Layout';
+import * as S from './styles';
+import TableEspecialidade from './table/TableEspecialidade';
 
 function Especialidade() {
+  const router = useRouter();
+  const arrayEspecialidade: Array<EspecialidadeModel> = [
+    { id: '1', descricao: 'Ortopedia muscular', especialidade: 'Ortopedia' },
+    { id: '2', descricao: 'Ortopedia da perna', especialidade: 'Ortopedia' },
+    { id: '3', descricao: 'Ortopedia do braço', especialidade: 'Ortopedia' },
+    { id: '4', descricao: 'Ortopedia do mão', especialidade: 'Ortopedia' },
+    { id: '5', descricao: 'Ortopedia do joelho', especialidade: 'Ortopedia' },
+  ];
+
+  const deletes = (especialidade: EspecialidadeModel) => {
+    console.log(especialidade);
+  };
+
+  const edit = (especialidade: EspecialidadeModel) => {
+    router.push(`/medical/especialidades/nova-especialidade?id=${especialidade.id}`);
+  };
+
   return (
     <Layout title="Especialidades">
-      <Box display={'flex'} justifyContent={'space-between'}>
-        <div>Especialidades</div>
-        <Link href={'/medical/especialidades/nova-especialidade'}>
-          <Button variant="contained" style={{ backgroundColor: '#659e6d' }}>
-            Nova Especialidade
-          </Button>
-        </Link>
-      </Box>
+      <S.Container>
+        <S.NavContainer>
+          <div>Especialidades</div>
+          <Link href={'/medical/especialidades/nova-especialidade'}>
+            <Button variant="contained" style={{ backgroundColor: '#659e6d' }}>
+              Nova Especialidade
+            </Button>
+          </Link>
+        </S.NavContainer>
+
+        <TableEspecialidade
+          onEdit={edit}
+          onDelete={deletes}
+          especialidade={arrayEspecialidade}
+        ></TableEspecialidade>
+      </S.Container>
     </Layout>
   );
 }
