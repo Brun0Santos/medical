8381cd52d4493ad4ea5cc98dcca6334f14dc4085
Bruno Santos <bruno_st17@hotmail.com>
2024-01-09 22:38:21 -0300

    feat: adicionando o componente responsavel pela tabela de responsabilidades

--- src/components/especialidades/table/TableEspecialidade.tsx
+++ src/components/especialidades/table/TableEspecialidade.tsx
@@ -1,6 +1,6 @@
 'use client';
 import {
-  Button,
+  Avatar,
   Paper,
   Table,
   TableBody,
@@ -9,9 +9,11 @@ import {
   TableHead,
   TableRow,
 } from '@mui/material';
-import Link from 'next/link';
+import { MdDelete } from 'react-icons/md';
+import { MdModeEdit } from 'react-icons/md';
 
 import { Especialidade } from '../../../models/especialidade/especialidadeModel';
+import * as S from './styles';
 
 interface ListaEspecialidade {
   especialidade: Array<Especialidade>;
@@ -30,8 +32,9 @@ function TableEspecialidade({ especialidade, onEdit, onDelete }: ListaEspecialid
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
-          <TableRow>
-            <TableCell align="left">Nome</TableCell>
+          <TableRow style={{ backgroundColor: '#e8e8e8' }}>
+            <TableCell align="left">Img</TableCell>
+            <TableCell align="center">Nome</TableCell>
             <TableCell align="center">Descrição</TableCell>
             <TableCell align="center">Opções</TableCell>
           </TableRow>
@@ -54,28 +57,21 @@ function TableEspecialidade({ especialidade, onEdit, onDelete }: ListaEspecialid
 const RowEspecialidade = ({ especialidade, onEdit, onDelete }: EspecialidadeRowProps) => {
   return (
     <TableRow key={especialidade.id}>
-      <TableCell align="left">{especialidade.especialidade}</TableCell>
+      <TableCell align="left">
+        <Avatar alt="luciano" src={'https://randomuser.me/api/portraits/men/52.jpg'} />
+      </TableCell>
+      <TableCell align="center">{especialidade.especialidade}</TableCell>
       <TableCell align="center">{especialidade.descricao}</TableCell>
-      <TableCell align="center">
-        <Link href={'/medical/especialidades'}>
-          <Button
-            variant="contained"
-            style={{ backgroundColor: '#658eff', marginRight: '10px' }}
-            onClick={() => onEdit}
-          >
-            Editar
-          </Button>
-        </Link>
+      <TableCell align="left">
+        <S.ButtonGroup>
+          <S.ButtonEdit onClick={() => onEdit(especialidade)}>
+            <MdModeEdit style={{ color: '#25435b', fontSize: '17px' }} />
+          </S.ButtonEdit>
 
-        <Link href={'/medical/especialidades1'}>
-          <Button
-            variant="contained"
-            style={{ backgroundColor: '#fd4444' }}
-            onClick={() => onDelete}
-          >
-            Deletar
-          </Button>
-        </Link>
+          <S.ButtonDelete onClick={() => onDelete(especialidade)}>
+            <MdDelete style={{ color: '#fce0e3', fontSize: '17px' }} />
+          </S.ButtonDelete>
+        </S.ButtonGroup>
       </TableCell>
     </TableRow>
   );
