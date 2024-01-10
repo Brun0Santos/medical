3d7722cc1bb6f56858c9d6e532fdb82d69844f96
Bruno Santos <bruno_st17@hotmail.com>
2024-01-09 22:39:20 -0300

    feat: adicionando os estilos das especialidades

---
+++ src/components/especialidades/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 40px;
+`;
