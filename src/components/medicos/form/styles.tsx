124341c0893f33777f7c4779444cad13b74ee55f
Bruno Santos <bruno_st17@hotmail.com>
2024-01-10 20:25:38 -0300

    feat: configurando os estilos

---
+++ src/components/medicos/form/styles.tsx
@@ -0,0 +1,30 @@
+'use client';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 40px;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 30px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 40px;
+`;
+
+export const Button = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 40px;
+`;
