ba1c7ea4a70244f3b4ded8148ef617fb240cdbf4
Bruno Santos <bruno_st17@hotmail.com>
2024-01-12 23:33:54 -0300

    fix: corrigindo os estilos

---
+++ src/components/medicos/commom/styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ContainerRadio = styled.div`
+  margin-bottom: 10px;
+
+  div {
+    margin-bottom: 5px;
+  }
+
+  label {
+    display: inline-block;
+    margin-right: 10px;
+  }
+`;
