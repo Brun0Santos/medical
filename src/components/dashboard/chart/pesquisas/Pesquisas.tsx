cb0289e22e6cc364be937444e6ab464475502e01
Bruno Santos <bruno_st17@hotmail.com>
2024-01-13 22:30:18 -0300

    feat: adicionando o grafico de pesquisas

---
+++ src/components/dashboard/chart/pesquisas/Pesquisas.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+
+function Pesquisas() {
+  const data = [
+    { name: 'January', Pesquisas: 12 },
+    { name: 'February', Pesquisas: 19 },
+    { name: 'March', Pesquisas: 3 },
+    { name: 'April', Pesquisas: 5 },
+    { name: 'May', Pesquisas: 2 },
+    { name: 'June', Pesquisas: 3 },
+    { name: 'July', Pesquisas: 10 },
+  ];
+
+  return (
+    <LineChart width={500} height={250} data={data} margin={{ top: 20, right: 20 }}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="Pesquisas" stroke="#e20447" />
+    </LineChart>
+  );
+}
+
+export default Pesquisas;
