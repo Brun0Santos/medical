2779815cc7c11761e1370c4fa485e21ce12668e2
Bruno Santos <bruno_st17@hotmail.com>
2024-01-13 23:23:25 -0300

    feat: adicionando os estilos do grafico de paciente

---
+++ src/components/dashboard/chart/pacientes/styles.tsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+export const ContainerEstatisticas = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 11px;
+  /* box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); */
+  border-radius: 9px;
+`;
+
+export const ContainerNovosPacientes = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 390px;
+  height: 120px;
+  /* background-color: #796eff; */
+  border-radius: 7px;
+`;
+
+export const ContainerVelhosPacientes = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 390px;
+  height: 120px;
+
+  border-radius: 7px;
+`;
+
+export const NavImage = styled.span`
+  display: flex;
+  min-width: 55px;
+  justify-content: center;
+  align-items: center;
+  height: 70px;
+  width: 70px;
+  border-radius: 50%;
+  background-color: #e9f6fe;
+`;
+
+export const ImgPaciente = styled.span`
+  display: flex;
+  min-width: 55px;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+  background-color: #fff6e5;
+`;
+
+export const DivInfo = styled.div`
+  margin-left: 15px;
+  color: #000;
+`;
+
+export const DivTitle = styled.div`
+  font-weight: 700;
+  font-size: 19px;
+`;
+
+export const PercentualContainer = styled.div`
+  display: flex;
+  padding-bottom: 35px;
+  margin-left: 14px;
+  align-items: center;
+  justify-content: center;
+  color: #928aff;
+`;
+
+export const DivIcon = styled.div`
+  margin-right: 4px;
+`;
