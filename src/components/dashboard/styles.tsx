7e4c7058db03f228d39ff56cb410daf1c8b512ea
Bruno Santos <bruno_st17@hotmail.com>
2024-01-13 21:23:40 -0300

    feat: adicionando os estilos do dashboard principal

---
+++ src/components/dashboard/styles.tsx
@@ -0,0 +1,107 @@
+import styled from 'styled-components';
+
+export const ContainerEstatisticas = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Title = styled.div`
+  margin-bottom: 40px;
+  font-size: 30px;
+  font-weight: 500px;
+`;
+
+export const ContainerVendas = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  height: 120px;
+  background-color: #796eff;
+  border-radius: 7px;
+`;
+
+export const ContainerPacientes = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  height: 120px;
+  background-color: #ff5263;
+  border-radius: 7px;
+`;
+
+export const ContainerConsultoria = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  height: 120px;
+  background-color: #ffa900;
+  border-radius: 7px;
+`;
+
+export const ContainerVideo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  height: 120px;
+  background-color: #23a9f9;
+  border-radius: 7px;
+`;
+
+export const NavImage = styled.span`
+  display: flex;
+  min-width: 55px;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+  background-color: #9188ff;
+`;
+
+export const ImgPaciente = styled.span`
+  display: flex;
+  min-width: 55px;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+  background-color: #ff717f;
+`;
+
+export const ImgConsultoria = styled.span`
+  display: flex;
+  min-width: 55px;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+  background-color: #ffb82e;
+`;
+
+export const ImgVideo = styled.span`
+  display: flex;
+  min-width: 55px;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+  background-color: #4bb8fa;
+`;
+
+export const DivInfo = styled.div`
+  margin-left: 15px;
+  color: #fff;
+`;
+
+export const DivTitle = styled.div`
+  font-weight: bold;
+  font-size: 31px;
+`;
