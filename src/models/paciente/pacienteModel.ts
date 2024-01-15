a9be13ba9f4b603ee393f2fbd39a1debf76ad882
Bruno Santos <bruno_st17@hotmail.com>
2024-01-15 20:32:42 -0300

    feat: adicionando a entidade paciente

---
+++ src/models/paciente/pacienteModel.ts
@@ -0,0 +1,13 @@
+import { Address } from '../endereco/enderecoModel';
+
+export interface Patient {
+  id?: string;
+  name?: string;
+  contact?: string;
+  gender?: 'MALE' | 'FEMALE' | 'OTHER';
+  email?: string;
+  cpf?: string;
+  profession?: string;
+  medicalInsurance?: 'INDIVIDUAL' | 'FAMILIAR' | 'COLETIVO' | 'EMPRESARIAL';
+  address?: Address;
+}
