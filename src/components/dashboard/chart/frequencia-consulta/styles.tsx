38b3253463fb7e0558b9cffc01807c06274b2214
Bruno Santos <bruno_st17@hotmail.com>
2024-01-15 20:25:36 -0300

    feat: adicionando os estilos do grafico de frequencia de consultas

---
+++ src/components/dashboard/chart/frequencia-consulta/styles.tsx
@@ -0,0 +1,135 @@
+import styled from 'styled-components';
+
+export const ContainerEstatisticas = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Title = styled.div`
+  margin-bottom: 40px;
+  font-size: 30px;
+  font-weight: 500px;
+`;
+
+export const ContainerVendas = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  height: 120px;
+  background-color: #796eff;
+  border-radius: 7px;
+`;
+
+export const ContainerPacientes = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  height: 120px;
+  background-color: #ff5263;
+  border-radius: 7px;
+`;
+
+export const ContainerConsultoria = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  height: 120px;
+  background-color: #ffa900;
+  border-radius: 7px;
+`;
+
+export const ContainerVideo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  height: 120px;
+  background-color: #23a9f9;
+  border-radius: 7px;
+`;
+
+export const NavImage = styled.span`
+  display: flex;
+  min-width: 55px;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+  background-color: #9188ff;
+`;
+
+export const ImgPaciente = styled.span`
+  display: flex;
+  min-width: 55px;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+  background-color: #ff717f;
+`;
+
+export const ImgConsultoria = styled.span`
+  display: flex;
+  min-width: 55px;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+  background-color: #ffb82e;
+`;
+
+export const ImgVideo = styled.span`
+  display: flex;
+  min-width: 55px;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+  background-color: #4bb8fa;
+`;
+
+export const DivInfo = styled.div`
+  margin-left: 15px;
+  color: #fff;
+`;
+
+export const DivTitle = styled.div`
+  font-weight: bold;
+  font-size: 31px;
+`;
+
+export const ContainerGrafico = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 45px;
+  border-radius: 6px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+`;
+
+export const ContainerGraficoPizza = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 16px;
+  border-radius: 6px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+`;
+
+export const DivTitleGraficoPizza = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  font-weight: 400;
+  margin-top: 33px;
+`;
