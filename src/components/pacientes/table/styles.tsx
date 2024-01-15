85388ba595071ba597c8ba4a466dffb6bad42ec9
Bruno Santos <bruno_st17@hotmail.com>
2024-01-15 20:29:37 -0300

    feat: adicionando os estilos da tabela de medicos

---
+++ src/components/pacientes/table/styles.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const ButtonDelete = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  background: #ed5564;
+  cursor: pointer;
+  margin-right: 10px;
+`;
+
+export const ButtonEdit = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  background: #64b4f5;
+  cursor: pointer;
+  margin-right: 10px;
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: center;
+`;
