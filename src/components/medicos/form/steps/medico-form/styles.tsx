da2ac7579ad7fc1f08b06fb16b9b955dcc1e7db4
Bruno Santos <bruno_st17@hotmail.com>
2024-01-15 20:28:56 -0300

    feat: adicionando os estilos do form de medicos

---
+++ src/components/medicos/form/steps/medico-form/styles.tsx
@@ -0,0 +1,58 @@
+'use client';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 40px;
+`;
+
+export const ContentContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 30px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 40px;
+`;
+
+export const InputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+
+  input {
+    height: 50px;
+    margin-bottom: 20px;
+    padding: 0 15px;
+    border: 1px solid #ccc;
+    transition: border-color 0.3s ease-in-out;
+    outline: none;
+    border-radius: 3px;
+  }
+
+  :focus {
+    border-color: #1976d2;
+  }
+`;
+
+export const LabelInput = styled.label`
+  padding-bottom: 4px;
+`;
+
+export const CentroContainer = styled.form`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+
+  input {
+    width: 500px;
+  }
+`;
