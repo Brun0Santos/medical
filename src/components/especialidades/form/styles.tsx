1a1fedad81b1a7554c30d6731150744f2d819334
Bruno Santos <bruno_st17@hotmail.com>
2024-01-16 21:08:59 -0300

    fix: ajustando os estilos do form de especialidade

--- src/components/especialidades/form/styles.tsx
+++ src/components/especialidades/form/styles.tsx
@@ -13,7 +13,7 @@ export const NavContainer = styled.div`
   padding-bottom: 40px;
 `;
 
-export const ContentContainer = styled.div`
+export const ContentContainer = styled.form`
   display: flex;
   flex-direction: column;
   padding: 0px 30px;
@@ -23,8 +23,34 @@ export const ContentContainer = styled.div`
   padding-bottom: 40px;
 `;
 
-export const Button = styled.div`
+export const InputBox = styled.div`
   display: flex;
-  justify-content: space-between;
-  padding-bottom: 40px;
+  flex-direction: column;
+  flex-wrap: wrap;
+  margin-top: 14px;
+
+  input {
+    height: 50px;
+    margin-bottom: 20px;
+    padding: 0 15px;
+    border: 1px solid #ccc;
+    transition: border-color 0.3s ease-in-out;
+    outline: none;
+    border-radius: 5px;
+    margin-bottom: 0px;
+  }
+
+  .input-error {
+    outline: 1px solid rgb(255, 72, 72);
+  }
+`;
+
+export const LabelInput = styled.label`
+  padding-bottom: 4px;
+`;
+
+export const ErrorMessage = styled.span`
+  color: rgb(255, 72, 72);
+  font-size: 0.8rem;
+  padding-top: 1px;
 `;
