c650e6daf27b31d257561a4a1a8d68d35ee54076
Bruno Santos <bruno_st17@hotmail.com>
2024-01-17 21:55:32 -0300

    feat: adicionando o toast no arquivo principal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,13 @@ import '../../styles/Home.module.css';
 import '../../styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 }
