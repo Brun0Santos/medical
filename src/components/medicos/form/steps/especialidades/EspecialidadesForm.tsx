47d4fac0c7ab36cdc75d0f6737ac204b321f8178
Bruno Santos <bruno_st17@hotmail.com>
2024-01-19 21:40:51 -0300

    fix: modificando o componente de especialidade

--- src/components/medicos/form/steps/especialidades/EspecialidadesForm.tsx
+++ src/components/medicos/form/steps/especialidades/EspecialidadesForm.tsx
@@ -1,21 +1,53 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+'use client';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import { useEffect, useState } from 'react';
 
-import { useForm } from 'react-hook-form';
-
+import { useSpecialityService } from '../../../../../http';
+import { Speciality } from '../../../../../models/especialidade/especialidadeModel';
+import { Page } from '../../../../commom/pageable/especialidade/Speciality';
 import * as S from '../../styles';
 
 interface Testes {
-  datas: any;
-  updateFiledHandler: any;
+  speciality?: Speciality;
+  updateFiledHandler: (fieldName: string, value: string) => void;
 }
 
-export default function EspecialidadesForm({ datas, updateFiledHandler }: Testes) {
-  const { register, handleSubmit } = useForm();
+export default function EspecialidadesForm({ updateFiledHandler }: Testes) {
+  const [especialidades, setEspecialidades] = useState<Page<Speciality>>({
+    content: [],
+    first: 0,
+    number: 0,
+    size: 0,
+    totalElements: 0,
+    totalPages: 0,
+  });
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const handleSubmitForm = (data: any) => {
-    console.log('teste');
-    console.log(data);
+  const specialityService = useSpecialityService();
+
+  useEffect(() => {
+    try {
+      specialityService.getPageSpeciality('', 0, 6).then((res) => {
+        setEspecialidades(res);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const testess = (_: any, newValue: any) => {
+    console.log('oi');
+    console.log(newValue);
+
+    const testes = especialidades.content.find((e) => e.name == newValue);
+    console.log(testes);
+    if (testes) {
+      updateFiledHandler('name', String(testes.name));
+      updateFiledHandler('summary', String(testes.summary));
+      updateFiledHandler('description', String(testes.description));
+      updateFiledHandler('registrationDate', String(testes.registrationDate));
+    }
   };
 
   return (
@@ -24,18 +56,16 @@ export default function EspecialidadesForm({ datas, updateFiledHandler }: Testes
         <h3>Especialidade</h3>
       </S.NavContainer>
 
-      <S.ContentContainer onSubmit={handleSubmit(handleSubmitForm)}>
-        <S.InputBox>
-          <S.LabelInput htmlFor="name">Nome da Especialidade:</S.LabelInput>
-          <input
-            type="text"
-            id="name"
-            {...register('name')}
-            placeholder="Especialidade"
-            value={datas.name}
-            onChange={(e) => updateFiledHandler('name', e.target.value)}
-          />
-        </S.InputBox>
+      <S.ContentContainer>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={especialidades?.content.map((e) => e?.name)}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Especialidades" />}
+          fullWidth
+          onChange={testess}
+        />
       </S.ContentContainer>
     </S.Container>
   );
