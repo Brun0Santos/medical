c6c8befa7da5097ebc9e5600be075e080e82aeb3
Bruno Santos <bruno_st17@hotmail.com>
2024-01-20 23:41:25 -0300

    feat: adicionando o componente de select dos plano de saude

---
+++ src/components/commom/select/paciente/SelectOptions.tsx
@@ -0,0 +1,26 @@
+import React, { ChangeEvent, FC } from 'react';
+
+interface OptionsSelect {
+  value: string;
+  label: string;
+}
+
+interface SelectProps {
+  options: Array<OptionsSelect>;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
+}
+
+const SelectOptions: FC<SelectProps> = ({ options, value, onChange }) => {
+  return (
+    <select value={value} onChange={onChange}>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default SelectOptions;
