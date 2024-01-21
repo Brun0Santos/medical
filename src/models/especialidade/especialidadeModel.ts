e458297c74c823ea3766df1a65745a03a91ab74f
Bruno Santos <bruno_st17@hotmail.com>
2024-01-21 14:48:05 -0300

    feat: adicionando a model de especialidade

--- src/models/especialidade/especialidadeModel.ts
+++ src/models/especialidade/especialidadeModel.ts
@@ -3,6 +3,8 @@ import { Doctor } from '../medico/medicoModel';
 export interface Speciality {
   id?: string;
   name?: string;
+  summary?: string;
   description?: string;
+  registrationDate?: string;
   doctor?: Array<Doctor>;
 }
