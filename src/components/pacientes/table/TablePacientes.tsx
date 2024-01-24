611194b6f7061a9b29366f9550857f566075da7a
Bruno Santos <bruno_st17@hotmail.com>
2024-01-23 21:41:56 -0300

    feat: adicionando o componente da tabela de paciente

--- src/components/pacientes/table/TablePacientes.tsx
+++ src/components/pacientes/table/TablePacientes.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import {
-  Avatar,
   Paper,
   Table,
   TableBody,
@@ -10,24 +9,18 @@ import {
   TableHead,
   TableRow,
 } from '@mui/material';
-import { MdDelete, MdModeEdit } from 'react-icons/md';
 
 import { Patient } from '../../../models/paciente/pacienteModel';
-import * as S from './styles';
+import RowPaciente from './RowPaciente';
 
 interface PatientList {
   patient: Array<Patient>;
   onEdit: (patient: Patient) => void;
   onDelete: (patient: Patient) => void;
+  onInfo: (patient: Patient) => void;
 }
 
-interface PatientRowProps {
-  patient: Patient;
-  onEdit: (patient: Patient) => void;
-  onDelete: (patient: Patient) => void;
-}
-
-function TablePacientes({ patient, onEdit, onDelete }: PatientList) {
+function TablePacientes({ patient, onEdit, onDelete, onInfo }: PatientList) {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
@@ -35,50 +28,27 @@ function TablePacientes({ patient, onEdit, onDelete }: PatientList) {
           <TableRow style={{ backgroundColor: '#e8e8e8' }}>
             <TableCell align="left">Img</TableCell>
             <TableCell align="center">Nome</TableCell>
-            <TableCell align="center">CRM</TableCell>
-            <TableCell align="center">Especialidade</TableCell>
+            <TableCell align="center">Plano</TableCell>
             <TableCell align="center">Contato</TableCell>
-            <TableCell align="center">Email</TableCell>
             <TableCell align="center">CPF</TableCell>
             <TableCell align="center">Opções</TableCell>
           </TableRow>
         </TableHead>
+
         <TableBody>
           {patient.map((patient) => (
-            <RowPaciente key={patient.id} patient={patient} onEdit={onEdit} onDelete={onDelete} />
+            <RowPaciente
+              key={patient.id}
+              patient={patient}
+              onEdit={onEdit}
+              onDelete={onDelete}
+              onInfo={onInfo}
+            />
           ))}
         </TableBody>
       </Table>
     </TableContainer>
   );
 }
-
-const RowPaciente = ({ patient, onEdit, onDelete }: PatientRowProps) => {
-  return (
-    <TableRow key={patient.id}>
-      <TableCell align="left">
-        <Avatar alt="luciano" src={`https://randomuser.me/api/portraits/men/${patient.id}.jpg`} />
-      </TableCell>
-
-      <TableCell align="center">{patient.name}</TableCell>
-      <TableCell align="center">{patient.cpf}</TableCell>
-      <TableCell align="center">{patient.address?.state}</TableCell>
-      <TableCell align="center">{patient.contact}</TableCell>
-      <TableCell align="center">{patient.email}</TableCell>
-      <TableCell align="center">{patient.cpf}</TableCell>
-      <TableCell align="left">
-        <S.ButtonGroup>
-          <S.ButtonEdit onClick={() => onEdit(patient)}>
-            <MdModeEdit style={{ color: '#25435b', fontSize: '17px' }} />
-          </S.ButtonEdit>
-
-          <S.ButtonDelete onClick={() => onDelete(patient)}>
-            <MdDelete style={{ color: '#fce0e3', fontSize: '17px' }} />
-          </S.ButtonDelete>
-        </S.ButtonGroup>
-      </TableCell>
-    </TableRow>
-  );
-};
 
 export default TablePacientes;
