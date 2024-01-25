1401f07dc4c4f4145fec055281921eb79d5a3d0a
Bruno Santos <bruno_st17@hotmail.com>
2024-01-24 22:27:25 -0300

    feat: adicionando a tabela de registros

---
+++ src/components/registros/table/TabelaRegistro.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../../layout/Layout';
+import RegistroForm from '../form/RegistroForm';
+
+function TabelaRegistro() {
+  return (
+    <Layout title="Painel Administrativo">
+      <RegistroForm />
+    </Layout>
+  );
+}
+
+export default TabelaRegistro;
