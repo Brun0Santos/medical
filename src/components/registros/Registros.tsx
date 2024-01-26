d3a6046fbae148f3a734da6b63f5cf577c85b83d
Bruno Santos <bruno_st17@hotmail.com>
2024-01-25 21:39:31 -0300

    feat: ajustando o componente de registros

--- src/components/registros/Registros.tsx
+++ src/components/registros/Registros.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import { Button } from '@mui/material';
+import Link from 'next/link';
+import router from 'next/router';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+
+import { useRegisterService } from '../../http';
+import { Patient } from '../../models/paciente/pacienteModel';
+import { Registro } from '../../models/registro/registroModel';
+import Layout from '../layout/Layout';
+import * as S from './styles';
+import TabelaRegistro from './table/TabelaRegistro';
 
 function Registros() {
-  return <div></div>;
+  const [registro, setRegistro] = useState<Array<Registro>>([]);
+  const registroService = useRegisterService();
+
+  useEffect(() => {
+    try {
+      registroService.getALlRegister().then((data) => {
+        setRegistro(data);
+      });
+    } catch (error) {
+      toast.error('Algum erro inesperado aconteceu!');
+    }
+  }, []);
+
+  const deletes = (paciente: Patient) => {
+    console.log(paciente);
+  };
+
+  const edit = (dpaciente: Patient) => {
+    router.push(`/medical/pacientes/novo-paciente?id=${dpaciente.id}`);
+  };
+
+  const info = (patient: Patient) => {
+    router.push(`/medical/pacientes/info?id=${patient.id}`);
+  };
+
+  return (
+    <Layout title="Pacientes">
+      <S.Container>
+        <S.NavContainer>
+          <h3>Atividades</h3>
+          <Link href={'/medical/registros/novo'}>
+            <Button variant="contained" style={{ backgroundColor: '#659e6d' }}>
+              Novo Registro
+            </Button>
+          </Link>
+        </S.NavContainer>
+
+        <TabelaRegistro registro={registro} onEdit={edit} onDelete={deletes} onInfo={info} />
+      </S.Container>
+    </Layout>
+  );
 }
 
 export default Registros;
