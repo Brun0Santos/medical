0144f62beba452c15422553defa3dbba1eed7101
Bruno Santos <bruno_st17@hotmail.com>
2024-01-28 18:22:00 -0300

    adicionando as services dentro do index

--- src/http/index.tsx
+++ src/http/index.tsx
@@ -1,3 +1,5 @@
 export { useSpecialityService } from './service/especialidadeService';
+export { useLoginService } from './service/loginService';
 export { useDoctorService } from './service/medicoService';
 export { usePatienteService } from './service/pacienteService';
+export { useRegisterService } from './service/registroService';
