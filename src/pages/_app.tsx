57883b910caf366969a1a2ff7f8f6f222e19b564
Bruno Santos <bruno_st17@hotmail.com>
2024-01-31 21:28:00 -0300

    feat: adicionando o token provider no app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,20 @@ import '../../styles/Home.module.css';
 import '../../styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import { Toaster } from 'react-hot-toast';
+
+import { TokenProvider } from '../context/LoginContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
-      <Toaster />
+      <TokenProvider>
+        <SessionProvider session={pageProps.session}>
+          <Component {...pageProps} />
+          <Toaster />
+        </SessionProvider>
+      </TokenProvider>
     </>
   );
 }
