d683802a8dee07daa824b55e5352a98cdbd243c7
Bruno Santos <bruno_st17@hotmail.com>
2024-02-02 22:43:01 -0300

    feat: adicionando novas rotas no loginService

--- src/http/service/loginService.ts
+++ src/http/service/loginService.ts
@@ -1,6 +1,6 @@
 import { AxiosResponse } from 'axios';
 
-import { Login } from '../../models/login/loginModel';
+import { Login, LoginData } from '../../models/login/loginModel';
 import { httpCliente } from '../routes/routes';
 
 const resourceUrl: string = '/api/v1/auth';
@@ -16,7 +16,19 @@ export const useLoginService = () => {
     return response.data;
   };
 
+  const createLogin = async (dataLogin: LoginData): Promise<void> => {
+    const url = '/api/v1/loginDoctor';
+    await httpCliente.post<Login>(url, dataLogin);
+  };
+
+  const createLoginPatient = async (dataLogin: LoginData): Promise<void> => {
+    const url = '/api/v1/loginPatient';
+    await httpCliente.post<Login>(url, dataLogin);
+  };
+
   return {
     login,
+    createLogin,
+    createLoginPatient,
   };
 };
