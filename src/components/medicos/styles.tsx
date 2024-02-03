38e6985e3a080b6dfaf33d4a7769077ab1d18a42
Bruno Santos <bruno_st17@hotmail.com>
2024-02-02 22:34:49 -0300

    styles: ajustando os estilos do componente

--- src/components/medicos/styles.tsx
+++ src/components/medicos/styles.tsx
@@ -4,6 +4,14 @@ export const Container = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+
+  .slow {
+    &:hover {
+      background-color: #f9f9f9;
+      border: 0 solid #f9f9f9;
+      box-shadow: 0 0 10px rgba(0, 1, 0, 0.2);
+    }
+  }
 `;
 
 export const NavContainer = styled.div`
