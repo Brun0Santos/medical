bedefac70f6c95ca3a5ba613ee80787ec7ede8d8
Bruno Santos <bruno_st17@hotmail.com>
2024-02-03 17:00:11 -0300

    feat: adicionando algumas validacoes no menu

--- src/components/layout/dropdown/menu/MenuDropDown.tsx
+++ src/components/layout/dropdown/menu/MenuDropDown.tsx
@@ -1,5 +1,8 @@
 'use client';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 import Link from 'next/link';
+import toast from 'react-hot-toast';
 import { HiOutlineUser } from 'react-icons/hi2';
 import { IoCalendarNumberOutline, IoSettingsOutline } from 'react-icons/io5';
 import { MdOutlineLogout } from 'react-icons/md';
@@ -7,6 +10,14 @@ import { MdOutlineLogout } from 'react-icons/md';
 import * as S from './styles';
 
 function MenuDropDown() {
+  const disconect = () => {
+    delete axios.defaults.headers.common['Authorization'];
+    Cookies.remove('token');
+    toast('Desconectado!', {
+      icon: '👏',
+    });
+  };
+
   return (
     <S.MenuContainer>
       <ul>
@@ -38,14 +49,16 @@ function MenuDropDown() {
           <S.LiTitle>Settings</S.LiTitle>
         </S.LiInfo>
 
-        <S.LiInfo>
-          <S.NavImage>
-            <i>
-              <MdOutlineLogout style={{ fontSize: '17px' }} />
-            </i>
-          </S.NavImage>
-          <S.LiTitle>Logout</S.LiTitle>
-        </S.LiInfo>
+        <Link href={'/medical/sign-in'} legacyBehavior>
+          <S.LiInfo>
+            <S.NavImage>
+              <i>
+                <MdOutlineLogout style={{ fontSize: '17px' }} />
+              </i>
+            </S.NavImage>
+            <S.LiTitle onClick={disconect}>Logout</S.LiTitle>
+          </S.LiInfo>
+        </Link>
       </ul>
     </S.MenuContainer>
   );
