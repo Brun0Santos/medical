44d200c10b872ed60a9e4108a5352dd9feac3f3b
Bruno Santos <bruno_st17@hotmail.com>
2024-02-04 14:37:54 -0300

    feat: adicionando nova inteface

--- src/models/login/loginModel.ts
+++ src/models/login/loginModel.ts
@@ -1,7 +1,14 @@
 export interface Login {
-  id?: string;
-  login?: string;
-  email?: string;
-  token?: string;
-  role?: string;
+  userId: string;
+  login: string;
+  email: string;
+  token: string;
+  role: string;
+}
+
+export interface LoginData {
+  login: string;
+  email: string;
+  password: string;
+  cpf?: string;
 }
