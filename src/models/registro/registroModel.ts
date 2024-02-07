99f2ef9ed22cca9e555d8696325dfc5170b04d9b
Geovane <geovanne_supera@algartelecom.com.br>
2024-02-06 23:12:25 -0300

    fix: adicionando novo atributo

---
+++ src/models/registro/registroModel.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface Registro {
+  id?: string;
+  description?: string;
+  specialityId?: string;
+  doctorId?: string;
+  patientId?: string;
+  serviceDateTime?: string;
+  appointmentStatus?: string;
+  typeMedicalAppointment?: string;
+  file?: Array<File>;
+}
+
+export interface RegistroFromId {
+  id?: string;
+  description?: string;
+  name?: string;
+  serviceDateTime?: string;
+  appointmentStatus?: string;
+  typeMedicalAppointment?: string;
+}
+
+export interface RegistroFromDoutor {
+  id?: string;
+  name: string;
+  description: string;
+  typeMedicalAppointment: string;
+  email: string;
+  serviceDateTime: string;
+  appointmentStatus: string;
+  namePatient: string;
+  data?: any;
+}
