3ad63601c0bda376de50da0b2d68cf113d37adfa
Bruno Santos <bruno_st17@hotmail.com>
2024-02-07 22:58:41 -0300

    feat: adicionando novas interfaces

--- src/models/registro/registroModel.ts
+++ src/models/registro/registroModel.ts
@@ -8,7 +8,6 @@ export interface Registro {
   serviceDateTime?: string;
   appointmentStatus?: string;
   typeMedicalAppointment?: string;
-  file?: Array<File>;
 }
 
 export interface RegistroFromId {
