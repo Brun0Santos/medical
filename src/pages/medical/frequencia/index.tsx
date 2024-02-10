c810584dc65687a720d98693895506f53298544b
Bruno Santos <bruno_st17@hotmail.com>
2024-02-09 23:52:55 -0300

    feat: adicionando o componente de frequencia

---
+++ src/pages/medical/frequencia/index.tsx
@@ -0,0 +1,7 @@
+// import FrequenciaConsulta from '../../../components/dashboard/chart/frequencia-consulta/FrequenciaConsulta';
+
+// export default FrequenciaConsulta;
+
+import CalendarioComConsultas from '../../../components/data-registros/CalendarioComConsultas';
+
+export default CalendarioComConsultas;
