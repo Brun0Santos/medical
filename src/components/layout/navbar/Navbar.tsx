555fd94ea9b86a7e526cb8c6db7ade8caad26819
Bruno Santos <bruno_st17@hotmail.com>
2024-02-16 23:18:31 -0300

    fix: ajustando o token no carregamento do componente

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 'use client';
+import axios from 'axios';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import Image from 'next/image';
 import { useContext, useEffect, useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
 import { IoNotificationsSharp } from 'react-icons/io5';
 
-import Logo from '../../../../public/d.jpg';
 import { LoginContext } from '../../../context/LoginContext';
 import { useFileService } from '../../../http';
 import { AvatarDoctor } from '../../../models/avatar/avatarModel';
@@ -20,17 +21,67 @@ interface Title {
 function Navbar({ title, children }: Title) {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
   const [openNotification, setOpenNotification] = useState<boolean>(false);
-  const { token } = useContext(LoginContext);
   const service = useFileService();
   const [registrosPorDoutor, setRegistroPorDoutor] = useState<AvatarDoctor>();
+  const [userId, setUserId] = useState<string>();
+  const [tokens, setTokens] = useState<string>();
+  const [login, setLogin] = useState<string>();
+  const [role, setRole] = useState<string>();
+
+  const { setToken } = useContext(LoginContext);
+
+  const decodeToken = (token: string | null): JwtPayload | null => {
+    try {
+      if (!token) {
+        throw new Error('Token não fornecido');
+      }
+
+      const decoded = jwt.decode(token);
+
+      if (!decoded || typeof decoded === 'string') {
+        throw new Error('Token inválido');
+      }
+
+      return decoded as JwtPayload;
+    } catch (error) {
+      console.error('Erro ao decodificar o token:', error);
+      return null;
+    }
+  };
 
   useEffect(() => {
-    if (token?.userId) {
-      service.avatarFromDoutor(token.userId).then((data) => {
-        setRegistroPorDoutor(data);
-      });
+    if (userId) {
+      service
+        .avatarFromDoutor(userId)
+        .then((data) => {
+          setRegistroPorDoutor(data);
+        })
+        .catch(() => {});
     }
-  }, [token]);
+  }, [tokens]);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+
+    if (storedToken) {
+      setTokens(storedToken);
+      const decodedToken = decodeToken(storedToken);
+
+      setUserId(decodedToken?.userId);
+      setLogin(decodedToken?.sub);
+      setRole(decodedToken?.role);
+
+      setToken({
+        userId: decodedToken?.userId,
+        login: decodedToken?.login,
+        email: decodedToken?.email,
+        token: decodedToken?.token,
+        role: decodedToken?.role,
+      });
+
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + storedToken;
+    }
+  }, []);
 
   return (
     <S.ContentContainer>
@@ -48,23 +99,20 @@ function Navbar({ title, children }: Title) {
 
           <S.NavImage>
             <Image
-              src={'data:image;base64, ' + registrosPorDoutor?.file}
+              src={registrosPorDoutor?.file ? `data:image;base64,${registrosPorDoutor.file}` : ''}
               alt="Minha Imagem"
               width={300}
               height={300}
+              decoding="async"
             />
           </S.NavImage>
 
           <S.NameText onClick={() => setOpenMenu(!openMenu)}>
-            <h4>
-              {token?.login !== undefined
-                ? token.login.charAt(0).toUpperCase() + token.login.slice(1)
-                : ''}
-            </h4>
+            <h4>{login !== undefined ? login.charAt(0).toUpperCase() + login.slice(1) : ''}</h4>
             <div style={{ fontSize: '12px' }}>
-              {token?.role == 'ADMIN' && <span className="email">Administrador</span>}
-              {token?.role == 'PATIENT' && <span className="email">Paciente</span>}
-              {token?.role == 'DOCTOR' && <span className="email">Médico</span>}
+              {role == 'ADMIN' && <span className="email">Administrador</span>}
+              {role == 'PATIENT' && <span className="email">Paciente</span>}
+              {role == 'DOCTOR' && <span className="email">Médico</span>}
               <IoIosArrowDown style={{ marginLeft: '10px', fontSize: '15px' }} />
               {openMenu && <MenuDropDown />}
             </div>
