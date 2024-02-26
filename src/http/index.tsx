f19acbe296b8ba706a86d511d76b98be46131759
Bruno Santos <bruno_st17@hotmail.com>
2024-02-25 21:43:28 -0300

    feat: exportando as services

--- src/http/index.tsx
+++ src/http/index.tsx
@@ -1,4 +1,5 @@
 export { useSpecialityService } from './service/especialidadeService';
+export { useFileService } from './service/fileService';
 export { useLoginService } from './service/loginService';
 export { useDoctorService } from './service/medicoService';
 export { usePatienteService } from './service/pacienteService';
