0c424dbf261a6eaed93eccbe27f18d8ebfea873e
Bruno Santos <bruno_st17@hotmail.com>
2024-02-26 22:49:33 -0300

    fix: ajustando a service de arquivos

--- src/http/service/fileService.ts
+++ src/http/service/fileService.ts
@@ -8,7 +8,7 @@ const resourceUrl: string = '/api/v1/avatar/doctor';
 
 export const useFileService = () => {
   const saveRegister = async (formData: FormData): Promise<void> => {
-    const url: string = `http://localhost:8080/api/v1/avatar/doctor`;
+    const url: string = `http://localhost:8080/api/v1/avatar/doctors`;
     await axios.post(url, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
